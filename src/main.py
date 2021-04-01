e6b1eaf8034f0487a7b4487ff804581130ef16f7
Thao Nguyen <thao.ngch@gmail.com>
2021-04-01 11:28:43 +0700

    Ini

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+print("hey guys")
+
+def _sum(a, b): 
+    sum_result = a + b
+    return sum_result
+
+def _mul(a, b):
+    multiple_result = a * b
+    return multiple_result
---
+++ tests/test_main.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append('../src/')
+
+import pytest
+import main
+
+def test_sum():
+    assert main._sum(1,2) == 3
+
+def test_mul():
+    assert main._mul(1,2) == 2
+
+def test_subtract():
+    assert 1 - 2 == -1
+
+@pytest.mark.xfail
+def test_xfail():
+    assert 1-2  == -5
+
+@pytest.mark.skip
+def test_xskip():
+    assert 1 -3 == 123
